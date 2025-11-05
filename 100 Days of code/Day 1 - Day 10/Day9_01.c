95e6f010aef2db21bde1be27b8c450643f6b5d33
Lakshya Kulshrestha <kulshresthalakshya2006@gmail.com>
2025-11-05 22:29:20 +0530

    Add files via upload

---
+++ 100 Days of code/Day 1 - Day 10/Day9_01.c
@@ -0,0 +1,39 @@
+// Write a program to find the roots of a quadratic equation and categorize them.
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c;
+    printf("Enter coefficients a, b and c: ");
+    // ax^2+bx+c=0 // 
+    scanf("%f %f %f", &a, &b, &c);
+
+    float discriminant, root1, root2, realPart, imagPart;
+    discriminant = b*b - 4*a*c; 
+
+    if (discriminant > 0)
+    {
+        printf("Roots are real and different.\n");
+        root1 = ((-b + sqrt(discriminant)) / (2*a));
+        root2 = ((-b - sqrt(discriminant)) / (2*a));
+        printf("Root 1 = %.2f\n", root1);
+        printf("Root 2 = %.2f\n", root2);
+    }
+    else if (discriminant == 0)
+    {
+        printf("Roots are real and the equal.\n");
+        root1 = root2 = (-b / (2*a));
+        printf("Root 1 = Root 2 = %.2f\n", root1);
+    }
+    else
+    {
+        printf("Roots are complex and different.\n");
+        realPart = -b / (2*a);
+        imagPart = sqrt(-discriminant) / (2*a);
+        printf("Root 1 = %2f + %2fi\n", realPart, imagPart);
+        printf("Root 2 = %2f - %2fi\n", realPart, imagPart);
+    }
+    return 0;
+}
