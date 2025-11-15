9830cb5e2a43e0b084adcb531bb10f500fa89160
Lakshya Kulshrestha <kulshresthalakshya2006@gmail.com>
2025-11-15 18:34:44 +0530

    palindrome

---
+++ 100 Days of code/Day 11 - Day 20/Day16_02.c
@@ -0,0 +1,34 @@
+// Q32: Write a program to check if a number is a palindrome.
+
+#include <stdio.h>
+
+int main() 
+{
+    int n, reversed = 0, original, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) 
+    {
+        printf("Negative numbers cannot be palindromes.\n");
+        return 0;
+    }
+
+    original = n;
+
+    while (n > 0) 
+    {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n = n / 10;
+    }
+
+    if (original == reversed) {
+        printf("%d is a palindrome.\n", original);
+    } else {
+        printf("%d is not a palindrome.\n", original);
+    }
+
+    return 0;
+}
