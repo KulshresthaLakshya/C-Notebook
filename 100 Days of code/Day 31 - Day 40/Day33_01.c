b9f8584fd5942fbf441edc5b408daff8967a7d6b
Lakshya Kulshrestha <kulshresthalakshya2006@gmail.com>
2025-11-27 20:16:12 +0530

    binary search implementation
    Implement binary search algorithm to find a key in a sorted array.

---
+++ 100 Days of code/Day 31 - Day 40/Day33_01.c
@@ -0,0 +1,39 @@
+// Search in a sorted array using binary search.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int key;
+    scanf("%d", &key);
+
+    int low = 0, high = n - 1;
+    int found = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            found = mid;
+            break;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (found != -1)
+        printf("Found at index %d", found);
+    else
+        printf("-1");
+
+    return 0;
+}
